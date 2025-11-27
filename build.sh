2827065133ebae65955b01daef3e9200e83f681e
Gerrit Jansen van Vuuren <475713+gerritjvv@users.noreply.github.com>
2025-11-27 16:58:17 +0100

    add build commands and test

--- build.sh
+++ build.sh
@@ -11,6 +11,17 @@ case $CMD in
  lint )
    go vet ./...
    ;;
+ test )
+   go test ./...
+   ;;
+ build )
+  echo "build amd64 mongo2s3-amd64"
+  GOOS=linux GOARCH=amd64 go build -o mongo2s3-amd64 ./cmd/
+  echo "build arm mongo2s3-arm64"
+  GOOS=linux GOARCH=arm64 go build -o mongo2s3-arm64 ./cmd/
+  echo "build macos mongo2s3-darwin-arm64"
+  GOOS=darwin GOARCH=arm64 go build -o mongo2s3-darwin-arm64 ./cmd
+  ;;
  * )
-   echo "Undefined command $CMD try (lint, build)"
+   echo "Undefined command $CMD try (lint, test, build)"
 esac
